3c134f4db87b9af334b9bd1230f5d4a167d21f26
Seroo <wtower123@gmail.com>
2019-07-12 00:20:07 +0900

    add more functions

--- FunctionalList.kt
+++ FunctionalList.kt
@@ -1,7 +1,3 @@
-package io.seroo
-
-import java.lang.IllegalArgumentException
-
 sealed class FunList<out T> {
     data class Cons<out T>(val head: T, val tail: FunList<T>) : FunList<T>()
     object Nil : FunList<Nothing>()
@@ -133,6 +129,63 @@ fun <T> FunList<T>.length(): Int {
     }
 }
 
-fun <T> T.println() {
-    kotlin.io.println(this)
+fun <T> FunList<T>.append(secondList: FunList<T> = FunList.Nil): FunList<T> {
+    return foldRight(secondList) { i, acc ->
+        FunList.Cons(i, acc)
+    }
+}
+
+fun <T> append2(firstList: FunList<T>, secondList: FunList<T>): FunList<T> {
+    return firstList.foldRight(secondList) { i, acc ->
+        FunList.Cons(i, acc)
+    }
+}
+
+fun <T> FunList<FunList<T>>.concat(): FunList<T> {
+    return foldRight(FunList.Nil, ::append2)
+}
+
+fun FunList<Int>.addOne(): FunList<Int> {
+    return foldLeft(FunList.Nil) { acc: FunList<Int>, i ->
+        FunList.Cons(i + 1, acc)
+    }.reverse()
+}
+
+fun <T, R> FunList<T>.map(transformer: (T) -> R): FunList<R> {
+    return foldLeft(FunList.Nil) { acc: FunList<R>, i ->
+        FunList.Cons(transformer(i), acc)
+    }.reverse()
+}
+
+fun <T> FunList<T>.filter(predicate: (T) -> Boolean): FunList<T> {
+    return foldLeft(FunList.Nil) { acc: FunList<T>, i ->
+        if (predicate(i)) {
+            FunList.Cons(i, acc)
+        } else {
+            acc
+        }
+    }
+}
+
+fun <T, R> FunList<T>.flatMap(transformer: (T) -> FunList<R>): FunList<R> {
+    return map(transformer).concat()
+}
+
+tailrec fun FunList<Int>.plus(secondList: FunList<Int>, acc: FunList<Int> = FunList.Nil): FunList<Int> {
+    return if (this !is FunList.Nil || secondList !is FunList.Nil) {
+        getTail().plus(secondList.getTail(), FunList.Cons(this.getHead() + secondList.getHead(), acc))
+    } else {
+        acc.reverse()
+    }
+}
+
+tailrec fun <T, R> FunList<T>.zipWith(secondList: FunList<T>, acc: FunList<R> = FunList.Nil, f: (T, T) -> R): FunList<R> {
+    return when {
+        this is FunList.Cons && secondList is FunList.Cons -> {
+            this.tail.zipWith(secondList.tail, FunList.Cons(f(head, secondList.head), acc), f)
+        }
+        else -> {
+            acc.reverse()
+        }
+    }
 }
