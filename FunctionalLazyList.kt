88e5ecd53f49dbf617c5c92679f7c96a6f151021
gyooha <wtower123@gmail.com>
2019-07-16 22:07:24 +0900

    add lazy list functions

--- FunctionalLazyList.kt
+++ FunctionalLazyList.kt
@@ -1,5 +1,5 @@
 sealed class Stream<out T> {
-    data class Cons<T>(val head: () -> T, val tail: () -> Stream<T>) : Stream<T>()
+    data class Cons<out T>(val head: () -> T, val tail: () -> Stream<T>) : Stream<T>()
     object Empty : Stream<Nothing>()
 
     companion object {
@@ -19,6 +19,54 @@ sealed class Stream<out T> {
 
 fun <T> empty(): Stream<T> = Stream.Empty
 
-fun <T> apply(list: List<T>): Stream<T> {
-    return if (list.isEmpty()) empty() else Stream.cons({ list[0] }, { apply(list.drop(1)) })
+fun <T> List<T>.apply2(): Stream<T> {
+    return if (isEmpty()) empty() else Stream.cons({ this[0] }, { drop(1).apply2() })
+}
+
+fun <T, R> Stream<T>.foldRight(acc: () -> R, f: (T, () -> R) -> R): R = when (this) {
+    is Stream.Cons -> {
+        f(head()) { tail().foldRight(acc, f) }
+    }
+    is Stream.Empty -> acc()
+}
+
+fun <T> Stream<T>.reverse(): Stream<T> {
+    return foldRight({ Stream.Empty }) { i: T, acc: () -> Stream<T> ->
+        Stream.Cons({ i }, acc)
+    }
+}
+
+fun <T, R> Stream<T>.map(transformer: (T) -> R): Stream<R> =
+        foldRight({ Stream.Empty }) { i: T, acc: () -> Stream<R> ->
+            Stream.Cons({ transformer(i) }, acc)
+        }
+
+fun <T> Stream<T>.toList() =
+        foldRight({ listOf() }) { i: T, acc: () -> List<T> ->
+            acc().plus(i)
+        }.reversed()
+
+fun <T> Stream<T>.filter(predicate: (T) -> Boolean): Stream<T> {
+    return foldRight({ Stream.Empty }) { i: T, acc: () -> Stream<T> ->
+        if (predicate(i)) {
+            Stream.Cons({ i }, acc)
+        } else {
+            acc()
+        }
+    }
+}
+
+fun <T> append(firstList: Stream<T>, targetList: () -> Stream<T>) =
+        firstList.foldRight(targetList) { i: T, acc: () -> Stream<T> ->
+            Stream.Cons({ i }, acc)
+        }
+
+fun <T> Stream<Stream<T>>.flatten() =
+        foldRight({ Stream.Empty }, ::append)
+
+fun <T, R> Stream<T>.flatMap(transformer: (T) -> Stream<R>): Stream<R> =
+        map(transformer).flatten()
+
+fun <T> T.println() {
+    println(this)
 }
